3bf00cc5b09355b97858341287d0cebe7a89afd5
taha soumbate <SOUMBATE.TAHA@ofppt-edu.ma>
2023-03-08 12:56:30 +0100

    css

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ let p2 = document.querySelector("#p2");
 
 color1.addEventListener("input", () => {
   body.style = `background:linear-gradient(to right,${color1.value},${color2.value}`;
+  color1.style = `background-color:${color1.value}`;
   p1.innerText = color1.value;
 });
 color2.addEventListener("input", () => {
   body.style = `background:linear-gradient(to right,${color1.value},${color2.value}`;
+  color2.style = `background-color:${color2.value}`;
   p2.innerText = color2.value;
 });
