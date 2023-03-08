56a845f3bb38dc47d1e43d1cf43ce75fe50953ef
taha soumbate <SOUMBATE.TAHA@ofppt-edu.ma>
2023-03-08 12:53:40 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let color1 = document.querySelector("#color1");
+let color2 = document.querySelector("#color2");
+let body = document.querySelector("body");
+let p1 = document.querySelector("#p1");
+let p2 = document.querySelector("#p2");
+
+color1.addEventListener("input", () => {
+  body.style = `background:linear-gradient(to right,${color1.value},${color2.value}`;
+  p1.innerText = color1.value;
+});
+color2.addEventListener("input", () => {
+  body.style = `background:linear-gradient(to right,${color1.value},${color2.value}`;
+  p2.innerText = color2.value;
+});
